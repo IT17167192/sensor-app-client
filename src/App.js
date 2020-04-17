8a0d8afb8638456c95b8e04cce77a119c7b4431f
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-04-18 01:43:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Nav/Navbar';
+import Sensors from './SensorMain/Sensors'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+      <Navbar/>
+      <br/>
+      <br/>
+      <Sensors/>
     </div>
   );
 }
